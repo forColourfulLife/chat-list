d07f789ce73179138e6a48eed5f088e7dd6a8eab
刘冰 <liubing@taofen8.com>
2021-03-07 20:17:39 +0800

    首次提交

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import infiniteScroll from 'vue-infinite-scroll'
+
+Vue.use(infiniteScroll)
+
+if (process.env.NODE_ENV === 'development') {
+  require('./mock/index.js')
+}
 
 Vue.config.productionTip = false
 
